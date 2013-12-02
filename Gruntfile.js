45ff3efdef888467187a7a2549e66a236e16821a
Ibe Vanmeenen <ibevanmeenen@hotmail.com>
2013-12-02 17:46:03 +0100

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -121,7 +121,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-uncss');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-svg2png');
