6ca8a182db9794f88a167e6b72669eb01e46bd2a
Ibe Vanmeenen <ibe.vanmeenen@sweetmustdard.be>
2018-05-13 15:55:42 +0200

    update

---
+++ houdini/demos/paint/demo-2/circle-worklet.js
@@ -0,0 +1,17 @@
+registerPaint(
+  'circle',
+  class {
+    static get inputProperties() {}
+
+    paint(ctx, size, properties) {
+      const x = size.width / 2;
+      const y = size.height / 2;
+      const radius = Math.min(x, y);
+
+      ctx.fillStyle = '#454468';
+      ctx.beginPath();
+      ctx.arc(x, y, radius, 0, 2 * Math.PI);
+      ctx.fill();
+    }
+  }
+);
