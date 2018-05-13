6fe823911946d9e3501da5da869e2284fa33c39d
Ibe Vanmeenen <ibe.vanmeenen@sweetmustdard.be>
2018-05-13 16:35:34 +0200

    update

--- houdini/demos/paint/demo-2/circle.paint-module.js
+++ houdini/demos/paint/demo-2/circle.paint-module.js
@@ -1,14 +1,17 @@
 registerPaint(
   'circle',
   class {
-    static get inputProperties() {}
+    static get inputProperties() {
+      return ['--circle-color'];
+    }
 
     paint(ctx, size, properties) {
+      const fill = properties.get('--circle-color');
       const x = size.width / 2;
       const y = size.height / 2;
       const radius = Math.min(x, y);
 
-      ctx.fillStyle = '#454468';
+      ctx.fillStyle = fill;
       ctx.beginPath();
       ctx.arc(x, y, radius, 0, 2 * Math.PI);
       ctx.fill();
